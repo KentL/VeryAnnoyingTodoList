20d61e535d0cf33ac01f4f7d1d8a7e0e5188e39c
kli <kentlee.zl1993@gmail.com>
2019-03-14 10:56:47 -0700

    Initial Commit

---
+++ VeryAnnoyingTodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VeryAnnoyingTodoList
+//
+//  Created by Kent Li on 2019-03-14.
+//  Copyright © 2019 Kent Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ VeryAnnoyingTodoListTests/VeryAnnoyingTodoListTests.swift
@@ -0,0 +1,34 @@
+//
+//  VeryAnnoyingTodoListTests.swift
+//  VeryAnnoyingTodoListTests
+//
+//  Created by Kent Li on 2019-03-14.
+//  Copyright © 2019 Kent Li. All rights reserved.
+//
+
+import XCTest
+@testable import VeryAnnoyingTodoList
+
+class VeryAnnoyingTodoListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
